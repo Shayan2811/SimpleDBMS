19a280034382d942654e68ecaa4b713d73d4d249
Shayan Rashid <164961724+Shayan2811@users.noreply.github.com>
2024-04-28 11:34:00 -0700

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,22 @@
+-- Create a database
+CREATE DATABASE SimpleDBMS;
 
+-- Use the database
+USE SimpleDBMS;
+
+-- Create a table for users
+CREATE TABLE `Users` (
+    `id` INT AUTO_INCREMENT,
+    `Username` VARCHAR(50) NOT NULL,
+    `Email` VARCHAR(100) NOT NULL,
+    `Age` INT,
+    PRIMARY KEY (`id`)
+);
+
+-- Create a table for products
+CREATE TABLE `Products` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `ProductName` VARCHAR(100) NOT NULL,
+    `Price` DECIMAL(10, 2) NOT NULL,
+    PRIMARY KEY (`id`)
+);
