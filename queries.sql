1bec75faf7a6a79f6187bfcd99f598acfab57b7c
Shayan Rashid <164961724+Shayan2811@users.noreply.github.com>
2024-04-28 11:30:56 -0700

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,1 +1,14 @@
+-- Insert data into the Users table
+INSERT INTO `Users` (`Username`, `Email`, `Age`)
+VALUES ('JohnDoe', 'johndoe@example.com', 30);
 
+-- Select all users
+SELECT * FROM `Users`;
+
+-- Update user information
+UPDATE `Users` SET `Age` = 35
+WHERE `Username` = 'JohnDoe';
+
+-- Delete a user
+DELETE FROM `Users`
+WHERE `UserID` = 1;
